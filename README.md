1fe146a86ebe2247d05e9b024c2cc0e035ee11d5
hezedu <hezedu@hotmail.com>
2016-01-23 10:50:25 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# dingtalk_SSO
-钉钉免登，套件和企业号通用。
+# dingtalk sso
+钉钉免登接口，ISV套件和企业号通用。
+
+##安装 
+`npm install dingtalk_sso`
+##示例
+构造函数：
+```js
+var dingtalk_sso = require('dingtalk_sso');
+var conf = {
+    corpid: 'dingxxxxxxxxxxxxxxx',
+    SSOSecret:'C1oXyeJUgH_QXEHYJS4-Um-zxfxxxxxxxxxxxxxxxxxx-6np3fXskv5dGs'
+  }
+var api = new dingtalk_sso(conf);
+```
+##方法
+### 通过CODE(免登授权码)换取用户身份
+```js
+api.getSSOUserInfoByCode(code, callback)
+```
+### 生成授权链接
+```js
+api.generateAuthUrl(redirect_url)
+```
+##更多钉钉相关
+ISV套件主动调用API: [dingtalk_suite](https://github.com/hezedu/dingtalk_suite)
+ISV套件回调serverAPI: [dingtalk_suite_callback](https://github.com/hezedu/dingtalk_suite_callback)
+企业号API: [dingtalk_enterprise](https://github.com/hezedu/dingtalk_enterprise)
---
+++ index.js
@@ -0,0 +1,48 @@
+var agent = require('superagent');
+var util = require('./util');
+
+var SSO_BASE_URL = 'https://oapi.dingtalk.com';
+var auth_url = 'https://oa.dingtalk.com/omp/api/micro_app/admin/landing?corpid=CORPID&redirect_url=REDIRECT_URL'
+
+var Api = function(conf) {
+  this.SSOSecret = conf.SSOSecret;
+  this.corpid = conf.corpid;
+}
+
+Api.prototype.getSSOToken = function(callback) {
+  var self = this;
+  console.log('corpid', self.corpid);
+  console.log('corpsecret', self.SSOSecret);
+  agent.get(SSO_BASE_URL + '/sso/gettoken')
+    .query({
+      corpid: self.corpid,
+      corpsecret: self.SSOSecret
+    })
+    .end(util.wrapper(callback));
+};
+
+//登录
+Api.prototype.getSSOUserInfoByCode = function(code, callback) {
+  var self = this;
+  self.getSSOToken(function(err, token) {
+    if (err) {
+      return callback(err);
+    };
+    console.log('SSO token', token);
+    agent.get(SSO_BASE_URL + '/sso/getuserinfo')
+      .query({
+        code: code,
+        access_token: token.access_token
+      })
+      .end(util.wrapper(callback));
+  });
+};
+
+//生成授权链接
+Api.prototype.generateAuthUrl = function(redirect_url) {
+  return 'https://oa.dingtalk.com/omp/api/micro_app/admin/landing?corpid=' + 
+  this.corpid + '&redirect_url=' + redirect_url;
+};
+
+
+module.exports = Api;
